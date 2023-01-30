48e6b4fad042166384b68750c2b9c28544bc8f80
bkr4 <87307553+bkr4@users.noreply.github.com>
2023-01-30 17:05:08 +0100

    wip

--- trurl-core/src/main/java/pl/edu/icm/trurl/ecs/util/EntitiesSampler.java
+++ trurl-core/src/main/java/pl/edu/icm/trurl/ecs/util/EntitiesSampler.java
@@ -1,44 +1,49 @@
 package pl.edu.icm.trurl.ecs.util;
 
 import net.snowyhollows.bento.annotation.WithFactory;
-import pl.edu.icm.trurl.ecs.Engine;
+import pl.edu.icm.trurl.ecs.EngineConfiguration;
 import pl.edu.icm.trurl.ecs.selector.Selector;
 import pl.edu.icm.trurl.store.Store;
 import pl.edu.icm.trurl.store.attribute.Attribute;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static pl.edu.icm.trurl.ecs.util.EntityIterator.select;
 
 @WithFactory
 public class EntitiesSampler {
-    private final Engine engine;
+    private final EngineConfiguration engineConfiguration;
 
 
-    public EntitiesSampler(Engine engine, Selectors selectors) {
-        this.engine = engine;
+    public EntitiesSampler(EngineConfiguration engineConfiguration, Selectors selectors) {
+        this.engineConfiguration = engineConfiguration;
     }
 
     public void copySelected(Selector selector, Store newStore) {
-        Map<Integer, Integer> idMap = createIdMapping(selector, newStore);
-        Store oldStore = engine.getStore();
-        for(Attribute attribute : newStore.attributes().collect(Collectors.toList())) {
-            for(int i=0; i < idMap.size(); i++) {
-                attribute.setString(i, oldStore.get(attribute.name()).getString(idMap.get(i)));
+        List<Integer> oldIds = createIdMapping(selector);
+        Store oldStore = engineConfiguration.getEngine().getStore();
+        for (Attribute attribute : newStore.attributes().collect(Collectors.toList())) {
+            if (attribute.name().equals("old_id")) {
+                for (int i = 0; i < oldIds.size(); i++) {
+                    attribute.setString(i, oldIds.get(i).toString());
+                }
+            } else {
+                for (int i = 0; i < oldIds.size(); i++) {
+                    attribute.setString(i, oldStore.get(attribute.name()).getString(oldIds.get(i)));
+                }
             }
+
         }
     }
 
-    private Map<Integer, Integer> createIdMapping(Selector selector, Store store) {
-        Map<Integer, Integer> map = new HashMap<>();
-        AtomicInteger i = new AtomicInteger(0);
-        engine.execute(select(selector).dontPersist().forEach(entity -> {
-            map.put(i.getAndIncrement(), entity.getId());
+    private List<Integer> createIdMapping(Selector selector) {
+        List<Integer> oldIds = new ArrayList<>();
+        engineConfiguration.getEngine().execute(select(selector).dontPersist().forEach(entity -> {
+            oldIds.add(entity.getId());
         }));
-        return map;
+        return oldIds;
     }
 }
 
