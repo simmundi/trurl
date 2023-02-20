77729515ac0ee451af5fcbf54bac9609085d2af5
bkr4 <87307553+bkr4@users.noreply.github.com>
2023-02-20 12:54:14 +0100

    add entities sampler

--- trurl-core/src/test/java/pl/edu/icm/trurl/ecs/EntitiesSamplerTest.java
+++ trurl-core/src/test/java/pl/edu/icm/trurl/ecs/EntitiesSamplerTest.java
@@ -3,37 +3,21 @@ package pl.edu.icm.trurl.ecs;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import pl.edu.icm.trurl.ecs.Engine;
-import pl.edu.icm.trurl.ecs.MapperSet;
-import pl.edu.icm.trurl.ecs.Session;
 import pl.edu.icm.trurl.ecs.mapper.Mapper;
 import pl.edu.icm.trurl.ecs.mapper.Mappers;
-import pl.edu.icm.trurl.ecs.selector.Chunk;
-import pl.edu.icm.trurl.ecs.selector.ChunkInfo;
-import pl.edu.icm.trurl.ecs.selector.Selector;
-import pl.edu.icm.trurl.ecs.util.*;
+import pl.edu.icm.trurl.ecs.util.ArraySelector;
+import pl.edu.icm.trurl.ecs.util.EntitiesSampler;
 import pl.edu.icm.trurl.exampledata.*;
-import pl.edu.icm.trurl.exampledata.pizza.Olive;
-import pl.edu.icm.trurl.exampledata.pizza.Pizza;
-import pl.edu.icm.trurl.exampledata.pizza.PizzaMapper;
 import pl.edu.icm.trurl.store.Store;
 import pl.edu.icm.trurl.store.array.ArrayStore;
 
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class EntitiesSamplerTest {
 
-    Store store = new ArrayStore(20);
     Store newStore = new ArrayStore(20);
 
     Person person1 = new Person();
@@ -48,7 +32,7 @@ class EntitiesSamplerTest {
     Mapper<Looks> looksMapper;
     Mapper<SomePoi> somePoiMapper;
 
-    @Mock
+    @Spy
     EngineConfiguration engineConfiguration;
 
     @BeforeEach
@@ -67,16 +51,10 @@ class EntitiesSamplerTest {
         looksMapper = Mappers.create(Looks.class);
         somePoiMapper = Mappers.create(SomePoi.class);
 
+        Store store = engineConfiguration.getEngine().getStore();
         personMapper.configureAndAttach(store);
         looksMapper.configureAndAttach(store);
         somePoiMapper.configureAndAttach(store);
-
-        when(engineConfiguration.getEngine()).thenReturn(new Engine(store, 5,
-                new MapperSet(new DynamicComponentAccessor(Person.class, Looks.class, SomePoi.class)), false));
-    }
-
-    @Test
-    void copySelected() {
 
         personMapper.save(person1, 1);
         personMapper.save(person2, 2);
@@ -87,8 +65,10 @@ class EntitiesSamplerTest {
         looksMapper.save(looks2, 1);
 
         somePoiMapper.save(poi, 0);
+    }
 
-        //execute
+    @Test
+    void copySelected() {
         EntitiesSampler sampler = new EntitiesSampler(engineConfiguration);
         ArraySelector selector = new ArraySelector();
         selector.addAll(new int[]{0, 1, 4});
@@ -98,6 +78,7 @@ class EntitiesSamplerTest {
 
         sampler.copySelected(selector, newStore);
 
-        assertThat(newStore.getCount()).isEqualTo(2);
+        assertThat(newStore.getCount()).isEqualTo(3);
+        assertThat(newStore.attributes().count()).isEqualTo(4);
     }
 }
