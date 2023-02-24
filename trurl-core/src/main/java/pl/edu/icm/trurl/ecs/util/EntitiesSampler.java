a0a2bb7e693df62ead5e75bc9b323224f2165c16
bkr4 <87307553+bkr4@users.noreply.github.com>
2023-02-24 09:00:02 +0100

    wip

--- trurl-core/src/main/java/pl/edu/icm/trurl/ecs/util/EntitiesSampler.java
+++ trurl-core/src/main/java/pl/edu/icm/trurl/ecs/util/EntitiesSampler.java
@@ -1,20 +1,30 @@
 package pl.edu.icm.trurl.ecs.util;
 
 import net.snowyhollows.bento.annotation.WithFactory;
+import pl.edu.icm.trurl.ecs.Engine;
 import pl.edu.icm.trurl.ecs.EngineConfiguration;
 import pl.edu.icm.trurl.ecs.selector.Selector;
 import pl.edu.icm.trurl.store.Store;
-import pl.edu.icm.trurl.store.attribute.Attribute;
+import pl.edu.icm.trurl.store.array.ValueObjectListArrayAttribute;
+import pl.edu.icm.trurl.store.attribute.EntityAttribute;
+import pl.edu.icm.trurl.store.attribute.EntityListAttribute;
+import pl.edu.icm.trurl.store.attribute.IntAttribute;
+import pl.edu.icm.trurl.store.attribute.ValueObjectListAttribute;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static pl.edu.icm.trurl.ecs.util.EntityIterator.select;
 
 
 public class EntitiesSampler {
     private final EngineConfiguration engineConfiguration;
+    private final AtomicInteger newIds = new AtomicInteger();
+    private int[] oldToNewIdMapping;
+    private List<Integer> newToOldIdMapping;
 
     @WithFactory
     public EntitiesSampler(EngineConfiguration engineConfiguration) {
@@ -22,30 +32,99 @@ public class EntitiesSampler {
     }
 
     public void copySelected(Selector selector, Store newStore) {
-        List<Integer> oldIds = createIdMapping(selector);
         Store oldStore = engineConfiguration.getEngine().getStore();
+        oldToNewIdMapping = createOldToNewIdMapping(selector);
+        newToOldIdMapping = createNewToOldIdMapping();
+        addAllRelatedEntities(oldStore, newStore);
 
-        for (Attribute attribute : newStore.attributes().collect(Collectors.toList())) {
-            for (int i = 0; i < oldIds.size(); i++) {
-                attribute.setString(i, oldStore.get(attribute.name()).getString(oldIds.get(i)));
+        newStore.attributes().forEach(attribute -> {
+            for (int newId = 0; newId < newToOldIdMapping.size(); newId++) {
+                if (attribute.getClass() == ValueObjectListArrayAttribute.class) {
+
+                    ValueObjectListArrayAttribute valueObjectListAttribute = oldStore.get(attribute.name());
+
+                    List<Integer> oldEntityIds = new ArrayList<>();
+                    valueObjectListAttribute.loadIds(newToOldIdMapping.get(newId), oldEntityIds::add);
+                    oldEntityIds.replaceAll(oldId -> oldToNewIdMapping[oldId]);
+                    StringBuilder newEntityIds = new StringBuilder();
+                    for (int i = 0; i < oldEntityIds.size(); i++) {
+                        if (i == 0) {
+                            newEntityIds.append(oldEntityIds.get(i));
+                        } else {
+                            newEntityIds.append(",").append(oldEntityIds.get(i));
+                        }
+                    }
+                    if (!newEntityIds.toString().equals("")) {
+                        ValueObjectListAttribute newValueObjectListAttribute = (ValueObjectListAttribute) attribute;
+                        newValueObjectListAttribute.setString(newId, newEntityIds.toString());
+                    }
+
+                } else {
+                    attribute.setString(newId, oldStore.get(attribute.name()).getString(newToOldIdMapping.get(newId)));
+                }
             }
-        }
+        });
 
         newStore.addInt("old_id");
-        Attribute oldIdsAttribute = newStore.get("old_id");
-        for (int i = 0; i < oldIds.size(); i++) {
-            oldIdsAttribute.setString(i, oldIds.get(i).toString());
+        IntAttribute oldIdsAttribute = newStore.get("old_id");
+        for (int i = 0; i < newToOldIdMapping.size(); i++) {
+            oldIdsAttribute.setInt(i, newToOldIdMapping.get(i));
         }
-        
-        newStore.fireUnderlyingDataChanged(0, oldIds.size());
+
+        newStore.fireUnderlyingDataChanged(0, newToOldIdMapping.size());
     }
 
-    private List<Integer> createIdMapping(Selector selector) {
-        List<Integer> oldIds = new ArrayList<>();
-        engineConfiguration.getEngine().execute(select(selector).dontPersist().forEach(entity -> {
-            oldIds.add(entity.getId());
+    private int[] createOldToNewIdMapping(Selector selector) {
+        Engine engine = engineConfiguration.getEngine();
+        int[] oldToNewMapping = new int[engine.getCount()];
+        Arrays.fill(oldToNewMapping, Integer.MIN_VALUE);
+        engine.execute(select(selector).dontPersist().forEach(entity -> {
+            oldToNewMapping[entity.getId()] = newIds.getAndIncrement();
         }));
-        return oldIds;
+        return oldToNewMapping;
+    }
+
+    private List<Integer> createNewToOldIdMapping() {
+        List<Integer> newToOldMapping = new ArrayList<>();
+        for (int oldId = 0; oldId < oldToNewIdMapping.length; oldId++) {
+            int newId = oldToNewIdMapping[oldId];
+            if (newId >= 0) {
+                newToOldMapping.add(newId, oldId);
+            }
+        }
+        return newToOldMapping;
+    }
+
+    private boolean addOldToNewIdMapping(int oldId) {
+        if (oldToNewIdMapping[oldId] >= 0) {
+            return false;
+        } else {
+            int newId = newIds.getAndIncrement();
+            oldToNewIdMapping[oldId] = newId;
+            newToOldIdMapping.add(newId, oldId);
+            return true;
+        }
+    }
+
+    private void addAllRelatedEntities(Store oldStore, Store newStore) {
+        AtomicBoolean changed = new AtomicBoolean(false);
+
+        do {
+            changed.set(false);
+            newStore.attributes().filter(a -> a.getClass() == ValueObjectListArrayAttribute.class)
+                    .forEach(attribute -> {
+                        for (int newId = 0; newId < newToOldIdMapping.size(); newId++) {
+                            ValueObjectListArrayAttribute valueObjectListAttribute = oldStore.get(attribute.name());
+
+                            List<Integer> oldEntityIds = new ArrayList<>();
+                            valueObjectListAttribute.loadIds(newToOldIdMapping.get(newId), oldEntityIds::add);
+
+                            oldEntityIds.forEach(oldId -> {
+                                changed.set(addOldToNewIdMapping(oldId));
+                            });
+                        }
+                    });
+        } while (changed.get());
     }
 }
 
--- trurl-core/src/test/java/pl/edu/icm/trurl/ecs/util/EntitiesSamplerTest.java
+++ trurl-core/src/test/java/pl/edu/icm/trurl/ecs/util/EntitiesSamplerTest.java
@@ -8,9 +8,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import pl.edu.icm.trurl.ecs.EngineConfiguration;
 import pl.edu.icm.trurl.ecs.mapper.Mapper;
 import pl.edu.icm.trurl.ecs.mapper.Mappers;
-import pl.edu.icm.trurl.ecs.util.ArraySelector;
-import pl.edu.icm.trurl.ecs.util.EntitiesSampler;
 import pl.edu.icm.trurl.exampledata.*;
+import pl.edu.icm.trurl.exampledata.pizza.*;
 import pl.edu.icm.trurl.store.Store;
 import pl.edu.icm.trurl.store.array.ArrayStore;
 
@@ -28,10 +27,13 @@ class EntitiesSamplerTest {
     Looks looks1 = new Looks();
     Looks looks2 = new Looks();
     SomePoi poi = new SomePoi();
+    Pizza pizzaA = new Pizza();
+    Pizza pizzaB = new Pizza();
 
     Mapper<Person> personMapper;
     Mapper<Looks> looksMapper;
     Mapper<SomePoi> somePoiMapper;
+    Mapper<Pizza> pizzaMapper;
 
     @Spy
     EngineConfiguration engineConfiguration;
@@ -48,14 +50,24 @@ class EntitiesSamplerTest {
         looks2.setColor(Color.SILVER);
         looks2.setTexture(Texture.ROUGH);
 
+        pizzaA.getOlives().add(Olive.of(OliveColor.BLACK, 1));
+        pizzaA.getToppings().add(Topping.of(Ingredient.BACON, 1));
+        pizzaA.getToppings().add(Topping.of(Ingredient.CHEESE, 10));
+        pizzaA.getToppings().add(Topping.of(Ingredient.ANCHOVIS, 1));
+        pizzaB.getOlives().add(Olive.of(OliveColor.GREEN, 1));
+        pizzaB.getOlives().add(Olive.of(OliveColor.BLACK, 2));
+
         personMapper = Mappers.create(Person.class);
         looksMapper = Mappers.create(Looks.class);
         somePoiMapper = Mappers.create(SomePoi.class);
+        pizzaMapper = Mappers.create(Pizza.class);
 
+        engineConfiguration.setInitialCapacity(20);
         Store store = engineConfiguration.getEngine().getStore();
         personMapper.configureAndAttach(store);
         looksMapper.configureAndAttach(store);
         somePoiMapper.configureAndAttach(store);
+        pizzaMapper.configureAndAttach(store);
 
         personMapper.save(person1, 1);
         personMapper.save(person2, 2);
@@ -66,20 +78,26 @@ class EntitiesSamplerTest {
         looksMapper.save(looks2, 1);
 
         somePoiMapper.save(poi, 0);
+
+        pizzaMapper.save(pizzaA, 5);
+        pizzaMapper.save(pizzaB, 6);
+
+        store.fireUnderlyingDataChanged(0, 7);
     }
 
     @Test
     void copySelected() {
         EntitiesSampler sampler = new EntitiesSampler(engineConfiguration);
         ArraySelector selector = new ArraySelector();
-        selector.addAll(new int[]{0, 1, 4});
+        selector.addAll(new int[]{0, 1, 4, 5, 6});
 
         personMapper.configureAndAttach(newStore);
         looksMapper.configureAndAttach(newStore);
+        pizzaMapper.configureAndAttach(newStore);
 
         sampler.copySelected(selector, newStore);
 
-        assertThat(newStore.getCount()).isEqualTo(3);
-        assertThat(newStore.attributes().count()).isEqualTo(4);
+        assertThat(newStore.getCount()).isEqualTo(7);
+        assertThat(newStore.attributes().count()).isEqualTo(11);
     }
 }
